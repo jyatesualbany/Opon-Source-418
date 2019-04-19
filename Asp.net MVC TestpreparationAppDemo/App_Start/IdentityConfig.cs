f8cd4e6d88422ddd83b2a75c05152bf1c267480e
DDavidson8899 <DDavidson@albany.edu>
2019-04-19 17:10:20 -0400

    password validation fix

--- Asp.net MVC TestpreparationAppDemo/App_Start/IdentityConfig.cs
+++ Asp.net MVC TestpreparationAppDemo/App_Start/IdentityConfig.cs
@@ -46,18 +46,18 @@ namespace Asp.net_MVC_TestpreparationAppDemo
             // Configure validation logic for usernames
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
-                AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
+                AllowOnlyAlphanumericUserNames = true,
+                RequireUniqueEmail = false
             };
 
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequiredLength = 1,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
